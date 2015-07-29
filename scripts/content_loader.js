7dca2b0fd2927f300408c56380e06b573f8ec530
athos <athos@vialink.com.br>
2015-07-29 09:53:37 -0300

    Tornando o content loader funcional. Criando suporte para histórico do browser.

--- scripts/content_loader.js
+++ scripts/content_loader.js
@@ -1,21 +1,35 @@
-function newClickBehavior() {
-  $("a").click(function(e) {
-    debugger;
-    e.preventDefault();
-    // More on history object on:
-    // https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history
-    history.pushState({}, "", e.currentTarget.href);
-    $("main").load("contents" + e.currentTarget.href, newClickBehavior);
-  });
+function load_path(URL) {
+  // Recieves the URL and returns the location of the requested content.
+  var data_path = URL;
+  if (URL == "/") {
+      data_path = "/home.html";
+  }
+  return "contents" + data_path;
+}
+
+function wrapper(selector) {
+  // Remove the default behavior of all "a" tags inside the selector object
+  // that are part of the class "local" and use jquery to load the website
+  // content via AJAX.
+  return function newClickBehavior() {
+    $(selector + " a.local").click(function(e) {
+      e.preventDefault();
+      history.pushState({}, "", e.currentTarget.pathname);
+      var path = e.currentTarget.pathname;
+      path = load_path(path);
+      $("main").load(path, wrapper("main"));
+    });
+  }
 }
 
 $(document).ready(function() {
   var path = window.location.pathname;
-  path = path.replace(/^(\/website\/)/,"");
-  if (path == "") {
-      path = "index.html";
-  }
-  $("nav").load("navbar.html");
-  $("footer").load("footer.html");
-  $("main").load("contents/" + path, newClickBehavior);
+  path = load_path(path);
+  $("nav").load("navbar.html", wrapper("nav"));
+  $("footer").load("footer.html", wrapper("footer"));
+  $("main").load(path, wrapper("main"));
 });
+
+window.onpopstate = function(event) {
+  $("main").load("contents" + event.target.location.pathname, wrapper("main"));
+};
